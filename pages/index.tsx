91d1aaa549ce687834fd4b49b9a780cea55556a0
Ivan De Menezes <ivandmenezes@gmail.com>
2019-12-07 02:37:18 -0400

    Show Todos on Index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import HeaderBar from "../components/header-bar/header-bar";
 import Content from "../components/content/content";
 import Container from "../components/container/container";
+import Todos from "../components/todos/todos";
 
 const Home = () => (
   <>
     <HeaderBar/>
     <Content>
       <Container>
-        hello
+        <Todos/>
       </Container>
     </Content>
   </>
