a67fe106ede40e73af063775646e5f84f72d04bb
Ivan De Menezes <ivandmenezes@gmail.com>
2019-12-07 01:27:02 -0400

    Add Content component.

---
+++ components/content/content.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Content = styled.main`
+  background-color: #fafafa;
+  height: calc(100vh - 60px);
+  position: relative;
+  padding: 0.75rem 0;
+`;
+
+export default Content;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import HeaderBar from "../components/header-bar/header-bar";
+import Content from "../components/content/content";
+import Container from "../components/container/container";
 
 const Home = () => (
   <>
     <HeaderBar/>
+    <Content>
+      <Container>
+        hello
+      </Container>
+    </Content>
   </>
 );
 
