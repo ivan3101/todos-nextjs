62aa016c3e35fb3d90b38b85e146bf1b9ddd94fb
Ivan De Menezes <ivandmenezes@gmail.com>
2019-12-07 01:41:26 -0400

    Add todos card Header.

---
+++ components/todos-header/todos-header.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const TodosHeader = styled.header`
+  background-color: #007eb4;
+  padding: 1.25rem;
+  
+  h2 {
+    color: #ffffff;
+    font-size: 18px;
+    font-weight: 500;
+  }
+`;
+
+export default TodosHeader;
