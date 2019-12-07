7821e59957f63befd7cce455ec5e6afc8f786e1d
Ivan De Menezes <ivandmenezes@gmail.com>
2019-12-07 02:10:02 -0400

    Change todos header styles.

--- components/todos-header/todos-header.tsx
+++ components/todos-header/todos-header.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const TodosHeader = styled.header`
   background-color: #007eb4;
-  padding: 1.25rem;
+  padding: 1.75rem 1.25rem;
   
   h2 {
     color: #ffffff;
